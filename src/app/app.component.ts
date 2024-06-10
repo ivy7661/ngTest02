4e00a58655c223650da7c22405a8a488de488be0
Ivy <ivy7661@gmail.com>
2024-06-10 21:59:14 +0800

    新增路由

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { StoreComponent } from './store/store.component';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, StoreComponent, HttpClientModule],
+  imports: [RouterOutlet, StoreComponent, HttpClientModule,RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { StoreComponent } from './store/store.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path:'',
+    component:AppComponent
+  },
+  {
+    path:'store',
+    component:StoreComponent
+  }
+];
