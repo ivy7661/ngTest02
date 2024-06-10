1d0d90f7769abee7a7ab38d880f84c88d997e7f9
Ivy <ivy7661@gmail.com>
2024-06-10 22:12:39 +0800

    add home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 import { StoreComponent } from './store/store.component';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
@@ -7,7 +8,7 @@ import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, StoreComponent, HttpClientModule,RouterModule],
+  imports: [RouterOutlet,HomeComponent, StoreComponent, HttpClientModule,RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,12 @@
 import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 import { StoreComponent } from './store/store.component';
 
 export const routes: Routes = [
   {
     path:'',
-    component:AppComponent
+    component:HomeComponent
   },
   {
     path:'store',
