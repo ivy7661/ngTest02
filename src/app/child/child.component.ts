aa92179d3fb4ac6e5ef817c77328f46b767c8431
Ivy <ivy7661@gmail.com>
2024-06-19 23:47:42 +0800

    local

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,5 +1,5 @@
-import { Component,Output,EventEmitter } from '@angular/core';
-import { Subject } from 'rxjs';
+import { Component,Output,EventEmitter, OnInit } from '@angular/core';
+import { Subject,fromEvent,Subscription,of } from 'rxjs';
 
 @Component({
   selector: 'app-child',
@@ -9,7 +9,7 @@ import { Subject } from 'rxjs';
   styleUrl: './child.component.scss'
 })
 
-export class ChildComponent {
+export class ChildComponent implements OnInit{
   @Output() messageEvent = new EventEmitter<string>();
   @Output() rxMessageEvent = new Subject<string>();
 
@@ -24,4 +24,40 @@ export class ChildComponent {
   changeColor() {
     this.isTrue=true;
   }
+  
+  public source:Subject<string>;
+  public clickSubscription!: Subscription;
+
+
+  constructor() {
+    this.source = new Subject<string>();
+  }
+
+  ngOnInit(): void {
+    this.source.subscribe({
+      next:value =>console.log(value),
+      error:err => console.log('Error:',err),
+      complete:() => console.log('complete')
+    })
+
+    this.source.next('hello');
+    this.source.next('world');
+
+    this.source.complete;
+
+
+    const source = fromEvent(document, 'click');
+    this.clickSubscription = source.subscribe((event: Event) => {
+      console.log('Document clicked!', event);
+    });
+  }
+
+  ngOnDestroy():void {
+    this.source.unsubscribe();
+    if (this.clickSubscription) {
+      this.clickSubscription.unsubscribe();
+    }
+  }
+
+
 }
