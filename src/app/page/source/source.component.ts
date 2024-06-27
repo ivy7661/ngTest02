935ab5dbb5c2a25fd53f22db034e331374fd8409
ivy <ivy7661@gmail.com>
2024-06-27 18:09:52 +0800

    defer

--- src/app/page/source/source.component.ts
+++ src/app/page/source/source.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { subscribe } from 'node:diagnostics_channel';
-import { fromEvent } from 'rxjs';
+import { Subscription, fromEvent,of,defer } from 'rxjs';
 
 
 @Component({
@@ -17,11 +17,22 @@ export class SourceComponent implements OnInit{
 
   ngOnInit(): void {
     fromEvent(document,'click').subscribe(data => {
-      console.log(`觸發!`);
       console.log(data);
-      
     })
   
+    // const subscription=
+    const promiseFactory = () => new Promise((resolve)=> {
+      console.log(`Promise被執行了`);
+      setTimeout(()=> {
+        resolve(100);
+      },1000)
+    })
 
+    const deferSource$ = defer(promiseFactory);
+    console.log(`示範用 defer 解決 Promise 問題：`);
+    deferSource$.subscribe(result => {
+      console.log(`Promise 結果:${result}`);
+    })
+    
   }
 }
