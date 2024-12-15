5e6f665046b19277f3accb4b65cf77740f7ee0e1
Ivy <ivy7661@gmail.com>
2024-12-16 00:00:22 +0800

    fix layout

--- src/app/page/source/source.component.ts
+++ src/app/page/source/source.component.ts
@@ -1,63 +1,51 @@
 import { AfterViewInit, Component,computed,ElementRef,HostListener,inject,OnInit, QueryList, signal, TemplateRef, ViewChild, ViewChildren, ViewContainerRef } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ComponentPortal, PortalModule } from '@angular/cdk/portal';
-import { Overlay, OverlayModule, OverlayRef } from '@angular/cdk/overlay';
-import { OverlayContentComponent } from '../../component/overlay-content/overlay-content.component';
-import { NgFor, NgIf } from '@angular/common';
-import { UserService } from '../../user.service';
-import { ParentComponent } from '../parent/parent.component';
+import { NgFor, NgIf, NgTemplateOutlet } from '@angular/common';
 @Component({
   selector: 'app-source',
   standalone: true,
-  imports: [ FormsModule, PortalModule, OverlayModule, NgIf, NgFor],
+  imports: [ FormsModule, PortalModule, NgIf, NgFor, NgTemplateOutlet],
   templateUrl: './source.component.html',
   styleUrl: './source.component.scss'
 })
-export class SourceComponent {
-  content: string[] = []; // 儲存模擬資料
-  isLoading = false;      // 加載狀態
+export class SourceComponent implements OnInit {
+  private timer: any = null;
 
-  private totalDataCount = 100;  // 總共的模擬資料數量
-  private loadedDataCount = 0;   // 已加載的資料數量
-  private pageSize = 15;         // 每次加載的資料數量
+  ngOnInit(): void {
 
-  constructor() {
-    this.loadMoreContent();
   }
 
-  // 滾動事件監聽
-  @HostListener('window:scroll', ['$event'])
-  onScroll() {
-    if (this.isLoading) return;
+  private performSearch(query: string): void {
+    console.log('Search for', query);
+    // call api
+  }
 
-    const scrollPosition = window.scrollY + window.innerHeight;
-    const bottomPosition = document.documentElement.scrollHeight;
+  public onSearch(value: string): void {
+    if (this.timer) {
+      clearTimeout(this.timer);
+    }
 
-    if (scrollPosition >= bottomPosition - 100) {
-      this.loadMoreContent();
+    this.timer = setTimeout(() => {
+      this.performSearch(value);
+      this.timer = null
+    }, 500)
+  }
+
+  public debounce(fn: Function, delay: number) {
+    let timer: ReturnType<typeof setTimeout> | null = null;
+    return (...args: any[]) => {
+      if (timer) {
+        clearTimeout(timer);
+      }
+  
+      timer = setTimeout(() => {
+        fn(...args);
+      }, delay)
     }
   }
-
-  // 模擬加載更多資料
-  loadMoreContent() {
-
-    if (this.loadedDataCount >= this.totalDataCount) return; // 如果所有數據都已加載，則停止
-    console.log('more data');
-
-    this.isLoading = true;
-
-    // 模擬 API 加載延遲
-    setTimeout(() => {
-      const newContent = this.generateMockData(this.loadedDataCount, this.pageSize);
-      this.content = [...this.content, ...newContent];
-      this.loadedDataCount += newContent.length;
-
-      this.isLoading = false;
-    }, 1000);
-  }
-
-  // 生成模擬資料
-  private generateMockData(startIndex: number, count: number): string[] {
-    return Array.from({ length: count }, (_, i) => `內容項目 ${startIndex + i + 1}`);
-  }
+  
+  public debounceSearch = this.debounce((value: string) => {
+    this.performSearch(value);
+  }, 500);
 }
